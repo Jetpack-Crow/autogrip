a252aa56c84fe6c020dcfaffd8fe7966a1d508b8
Jetpack_Crow <anthelion717@gmail.com>
2022-12-21 15:00:17 -0700

    Added an __init__ file so that this can be installed directly from the .zip like most add-ons

---
+++ __init__.py
@@ -0,0 +1,32 @@
+#----------------------------------------------------------
+# File __init__.py
+#----------------------------------------------------------
+ 
+#    Addon info
+bl_info = {
+    "name": "Autogrip",
+    "author": "Jetpack Crow, discord Jetpack Crow (they/them)#2281",
+    "version": (1, 0),
+    "blender": (3, 4, 00),
+    "location": "View3D > Extended Tools > AutoGrip",
+    "description": "Automatically poses hand rigs",
+    "category": '3D View'}
+if "bpy" in locals():
+    import imp
+    imp.reload(handrig)
+    print("Reloaded Autogrip")
+else:
+    from . import handrig
+    print("Imported Autogrip") 
+
+
+def register():
+    handrig.register()
+
+    
+def unregister():
+    handrig.unregister()
+
+    
+if __name__ == "__main__":
+    register()
